b9df24e1695591034657d4b9d742f77135846e1c
BONNe <bonne@bonne.id.lv>
2021-06-28 02:21:59 +0300

    Drop unnecessary 1.15-1.16 libs.
    As this version includes BentoBox 1.17, which requires Spigot 1.17, leaving old libs are just bloating the end jar.

--- src/main/java/lv/id/bonne/dragonfights/entity/CustomEntityAPI.java
+++ src/main/java/lv/id/bonne/dragonfights/entity/CustomEntityAPI.java
@@ -53,10 +53,6 @@ public class CustomEntityAPI
 	 */
 	static
 	{
-		versions.put(ServerVersion.v1_15, lv.id.bonne.dragonfights.v1_15_R1.NMSHandler::new);
-		versions.put(ServerVersion.v1_16_1, lv.id.bonne.dragonfights.v1_16_R1.NMSHandler::new);
-		versions.put(ServerVersion.v1_16_2, lv.id.bonne.dragonfights.v1_16_R2.NMSHandler::new);
-		versions.put(ServerVersion.v1_16_3, lv.id.bonne.dragonfights.v1_16_R3.NMSHandler::new);
 		versions.put(ServerVersion.v1_17, lv.id.bonne.dragonfights.v1_17_R1.NMSHandler::new);
 	}
 }
