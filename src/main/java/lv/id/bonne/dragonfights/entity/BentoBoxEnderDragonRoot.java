29185853b95949dbf6ffc6e411d415e938b68636
BONNe <bonne@bonne.id.lv>
2022-12-23 13:58:12 +0200

    Add Minecraft 1.19.3 support.

--- src/main/java/lv/id/bonne/dragonfights/entity/BentoBoxEnderDragonRoot.java
+++ src/main/java/lv/id/bonne/dragonfights/entity/BentoBoxEnderDragonRoot.java
@@ -91,7 +91,8 @@ public class BentoBoxEnderDragonRoot extends CompositeEntityRoot<EnderDragon>
 	{
 		this.setVersion(ServerVersion.v1_18_1, lv.id.bonne.dragonfights.v1_18_R1.entity.BentoBoxEnderDragonType::new);
 		this.setVersion(ServerVersion.v1_18_2, lv.id.bonne.dragonfights.v1_18_R2.entity.BentoBoxEnderDragonType::new);
-		this.setVersion(ServerVersion.v1_19, lv.id.bonne.dragonfights.v1_19_R1.entity.BentoBoxEnderDragonType::new);
+		this.setVersion(ServerVersion.v1_19_1, lv.id.bonne.dragonfights.v1_19_R1.entity.BentoBoxEnderDragonType::new);
+		this.setVersion(ServerVersion.v1_19_2, lv.id.bonne.dragonfights.v1_19_R2.entity.BentoBoxEnderDragonType::new);
 	}
 }
 
--- src/main/java/lv/id/bonne/dragonfights/entity/CustomEntityAPI.java
+++ src/main/java/lv/id/bonne/dragonfights/entity/CustomEntityAPI.java
@@ -55,6 +55,7 @@ public class CustomEntityAPI
 	{
 		versions.put(ServerVersion.v1_18_1, lv.id.bonne.dragonfights.v1_18_R1.NMSHandler::new);
 		versions.put(ServerVersion.v1_18_2, lv.id.bonne.dragonfights.v1_18_R2.NMSHandler::new);
-		versions.put(ServerVersion.v1_19, lv.id.bonne.dragonfights.v1_19_R1.NMSHandler::new);
+		versions.put(ServerVersion.v1_19_1, lv.id.bonne.dragonfights.v1_19_R1.NMSHandler::new);
+		versions.put(ServerVersion.v1_19_2, lv.id.bonne.dragonfights.v1_19_R2.NMSHandler::new);
 	}
 }
